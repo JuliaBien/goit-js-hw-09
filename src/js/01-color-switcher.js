b0a7f24ba3bb40d8da34f318496e667e0eb67fa4
JuliaBien <150686648+JuliaBien@users.noreply.github.com>
2024-04-17 13:28:11 +0200

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
-
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+let intervalId = null;
+const colorChanging = () => {
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startButton.setAttribute('disabled', '');
+};
+const stopColorChanging = () => {
+  clearInterval(intervalId);
+  startButton.removeAttribute('disabled');
+};
+startButton.addEventListener('click', colorChanging);
+stopButton.addEventListener('click', stopColorChanging);
