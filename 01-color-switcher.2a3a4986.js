919c485c009323bec3c86137b8deb499a28742b5
JuliaBien <150686648+JuliaBien@users.noreply.github.com>
2024-04-17 11:28:37 +0000

    Deploying to gh-pages from @ JuliaBien/goit-js-hw-09@b0a7f24ba3bb40d8da34f318496e667e0eb67fa4 🚀

---
+++ 01-color-switcher.2a3a4986.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");var o=null;t.addEventListener("click",(function(){o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.setAttribute("disabled","")})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.2a3a4986.js.map
